923d3cb870f9acea654104e6dd43240c20380951
kimpan0603 <bentsang1993@gmail.com>
2019-06-05 01:08:05 +0800

    Using ES7 to code JS

--- answer4.js
+++ answer4.js
@@ -2,12 +2,13 @@
 2. There is missing syntax(;) at the end of the row of arr[i].
 3. There is missing syntac(()) around x+i due to the instant function.
 4. There should be defined function first and arr[i] equals to the function name so arr[i] is defined as function.
+5. Using "var" to declare variable is not suitable. The variable will change globally.
 
 
 function createArrayOfFunctions(y){
-	var arr = [];
-	for(var i=0; i<y; i++){
-		cosnt createElement = x => x+i;
+	let arr = [];
+	for(let i=0; i<y; i++){
+		let createElement = x => x+i;
 		arr[i] = createElement;
 	}
 	return arr;
